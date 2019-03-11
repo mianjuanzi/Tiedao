6a624bceea677b41e0967d14f2ff0789f2fea98c
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-03-11 23:13:01 +0800

    qingbo's java code
    The first code by qingbo.

---
+++ qingbo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class qingbo {
+    public static void main(String args[]) throws IOException
+    {
+        int x=3,y=17;
+        int z;
+        z=x+y*x--;
+        System.out.print(z);
+    }
+}

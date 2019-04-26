84bc0da9b3a84454f6098e484856b2ac06093509
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-26 13:12:10 +0800

    Add files via upload

---
+++ L1_014.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName L1_014
+ * @Description 简单题
+ * @Author mianjuanzi
+ * @Date 2019/4/26 13:08
+ **/
+public class L1_014 {
+    public static void main(String[] args){
+        System.out.println("This is a simple problem.");
+    }
+}

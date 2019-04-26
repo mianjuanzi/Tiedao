2eade460dbccc83ef087c92f7df29e3abd665348
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-27 00:36:31 +0800

    Add files via upload

---
+++ L1_052.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName L1_052
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/4/27 0:24
+ **/
+public class L1_052 {
+    public static void main(String[] args){
+        System.out.println("2018\n" +
+                "wo3 men2 yao4 ying2 !");
+    }
+}

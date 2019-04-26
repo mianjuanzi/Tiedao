57168a58c0bf2bc152f3b31c6bd4822703967d6c
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-26 13:56:33 +0800

    Add files via upload

---
+++ L1_021.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName L1_021
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/4/26 13:54
+ **/
+public class L1_021 {
+    public static void main(String[] args){
+        System.out.println("I'm gonna WIN!\n" +
+                "I'm gonna WIN!\n" +
+                "I'm gonna WIN!");
+    }
+}

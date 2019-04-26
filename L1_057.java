374d0b7bd5c9e85fdb09d1392e59d5e7c3ff2bf7
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-26 14:01:29 +0800

    Add files via upload

---
+++ L1_057.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName L1_057
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/4/26 13:59
+ **/
+public class L1_057 {
+    public static void main(String[] args){
+        System.out.println("PTA shi3 wo3 jing1 shen2 huan4 fa1 !");
+    }
+}

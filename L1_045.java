19831287e62f5d1d51e27b16bfdd2805aa109aa0
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-27 00:10:58 +0800

    Add files via upload

---
+++ L1_045.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName L1_045
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/4/27 0:07
+ **/
+import java.util.Scanner;
+public class L1_045 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String str=scan.next();
+        System.out.println("Hello "+str);
+    }
+}

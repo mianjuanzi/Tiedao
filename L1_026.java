beec9866f3221c28738802396b838f2f39119f60
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-26 16:45:55 +0800

    Add files via upload

---
+++ L1_026.java
@@ -0,0 +1,21 @@
+/**
+ * @ClassName L1_026
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/4/26 16:39
+ **/
+public class L1_026 {
+    public static void main(String[] args){
+        System.out.println("I\n" +
+                " \n" +
+                "L\n" +
+                "o\n" +
+                "v\n" +
+                "e\n" +
+                " \n" +
+                "G\n" +
+                "P\n" +
+                "L\n" +
+                "T");
+    }
+}

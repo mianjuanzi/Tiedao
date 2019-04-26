121044366aff3f9dbb4d23dfa6ad3749120264bd
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-04-26 16:57:54 +0800

    Add files via upload

---
+++ L1_029.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName L1_029
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/4/26 16:49
+ **/
+import java.util.Scanner;
+public class L1_029 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int height=scan.nextInt();
+        double weight=(height-100)*0.9*2;
+        System.out.println(weight);
+    }
+}

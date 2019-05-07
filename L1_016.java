e12fdc02000282a29308025b9f87d9167f1db5b2
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-05-07 20:37:52 +0800

    Add files via upload

--- L1_016.java
+++ L1_016.java
@@ -2,25 +2,45 @@
  * @ClassName L1_016
  * @Description ToDo
  * @Author mianjuanzi
- * @Date 2019/4/26 13:15
+ * @Date 2019/4/30 11:49
  **/
 import java.util.Scanner;
 public class L1_016 {
-    public static void main(String[] args){
-        Scanner scan=new Scanner(System.in);
-        double m=scan.nextInt();
-        String a=scan.next();
-        char c=a.charAt(0);
-        int n=(int)Math.round(m/2);
-        for(int i=0;i<n;i++){
-            System.out.println(show((int)m,c));
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] id = new String[n];
+        int[] weight = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+        char[] m = {'1', '0', 'x','9', '8', '7', '6', '5', '4', '3', '2'};
+        for (int i = 0; i < n; i++) {
+            id[i] = scan.next();
         }
-    }
-    public static char[] show(int n,char c){
-        char [] ch=new char[n];
-        for(int i=0;i<n;i++){
-            ch[i]=c;
+        int error=0;
+        for (int i = 0; i < n; i++) {
+            boolean flag=true;
+            int sum = 0;
+            for (int j = 0; j < 17; j++) {
+                if (!(id[i].charAt(j) >= '0' && id[i].charAt(j) <= '9')) {
+                    flag = false;
+                    error++;
+                }
+                else {
+                    sum += ((id[i].charAt(j) - '0') * weight[j]);
+                }
+            }
+            if (flag&&(m[sum % 11] != id[i].charAt(17))) {
+                flag = false;
+                error++;
+            }
+            if(!flag){
+                System.out.println(id[i]);
+            }
         }
-        return ch;
+        if(error==0){
+            System.out.println("All passed");
+        }
+        else{
+            System.out.println("error: "+error);
+        }
     }
 }

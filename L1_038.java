d7d8e5b054004f0528dc2cabc75476370bdb1e73
mianjuanzi <48446137+mianjuanzi@users.noreply.github.com>
2019-05-14 21:24:55 +0800

    Add files via upload

---
+++ L1_038.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName L1_038
+ * @Description ToDo
+ * @Author mianjuanzi
+ * @Date 2019/5/14 21:21
+ **/
+public class L1_038 {
+    public static void main(String[] args){
+        System.out.println("Hello World\n" +
+                "Hello New World");
+    }
+}
